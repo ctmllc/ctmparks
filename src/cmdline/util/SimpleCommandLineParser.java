62189da6a251bda8b71fc30fe47f6a28ee244807
ctmllc <atit10_99@yahoo.com>
2014-03-28 13:56:41 -0500

    Made a small change. Purpose is to learn git command line.

--- src/cmdline/util/SimpleCommandLineParser.java
+++ src/cmdline/util/SimpleCommandLineParser.java
@@ -20,7 +20,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-//Simple change
+//Simple  change 
 /**
  * A simple class that provides utilities to ease command line parsing.
  *
